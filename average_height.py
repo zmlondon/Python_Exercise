11a3f1c04a7a20c1cdd6eebffa7d78e8c3124c46
Mu Zhao <zhaomu817@163.com>
2023-09-15 10:51:41 +0300

    correct mistakes

--- average_height.py
+++ average_height.py
@@ -1,7 +1,7 @@
 student_height = input("Input a list of student heights").split()
-student_height[n] = int(student_height[n])
-for n in student_height:
-    print(student_height)
+for n in range(0, len(student_height)):
+    student_height[n] = int(student_height[n])
+    #print(student_height)
 
 total_heights = 0
 for height in total_heights:
@@ -9,7 +9,7 @@ for height in total_heights:
     print(f"{total_heights}")
 
 number_of_students = 0
-for student in number_of_students:
+for student in student_height:
     number_of_students += 1
     print(f"{number_of_students}")
 
