48380678ed82b2a7503ed6f3789ae89a4a0944c4
Mu Zhao <zhaomu817@163.com>
2023-09-15 10:41:54 +0300

    test

---
+++ average_height.py
@@ -0,0 +1,17 @@
+student_height = input("Input a list of student heights").split()
+student_height[n] = int(student_height[n])
+for n in student_height:
+    print(student_height)
+
+total_heights = 0
+for height in total_heights:
+    total_heights += 0
+    print(f"{total_heights}")
+
+number_of_students = 0
+for student in number_of_students:
+    number_of_students += 1
+    print(f"{number_of_students}")
+
+average_height = round(total_heights/number_of_students)
+print(f"{average_height}")
